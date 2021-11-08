f901cc60a3b087ffdb18e1227c84e8d9390e2c15
Henrique de Souza Ando <Henrique.sando@corp.medgrupo.net>
2021-11-08 15:10:47 -0300

    Adicionado as urls,template, view e forms para create, update, delete e o requirements.

---
+++ clientes/form.py
@@ -0,0 +1,17 @@
+﻿from django import forms
+from django.db.models import fields
+from django.forms import ModelForm
+from .models import Cliente
+
+
+class ClienteForm(ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome','sobre_nome','dt_nasc','cpf','email','sexo'] 
+
+
+class ClienteUpdateForm(ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome','sobre_nome','dt_nasc','cpf','email','sexo','dt_alteracao','sn_ativo']
+        
--- clientes/views.py
+++ clientes/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import datetime
 from clientes.models import Cliente
+from .form import ClienteForm,ClienteUpdateForm
 # Create your views here.
 
 def home (request):
     now_tiem  = datetime.datetime.now()
-
     return render(request,'clientes/home.html')
 
 def list (request):
     data = {}
     data['clientes'] = Cliente.objects.all()
     return render(request,'clientes/list.html', data)
+
+
+def create(request):
+    data = {}
+    form = ClienteForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('lista')
+    data['form'] = form
+    return render(request,'clientes/form.html',data)
+
+
+def update(request,pk):
+    data = {}
+    cliente = Cliente.objects.get(pk=pk)
+    form = ClienteUpdateForm(request.POST or None, instance=cliente)
+    if form.is_valid():
+        form.save()
+        return redirect('lista')
+    data['form'] = form
+    data['cliente'] = cliente
+    return render(request,'clientes/form.html',data)
+
+def delete (request,pk):
+    cliente = Cliente.objects.get(pk=pk)
+    cliente.delete()
+    return redirect('lista')
